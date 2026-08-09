3f60482cf861d1317503ea11de0f4d2cfcfa4737
cadmiumbeast <shakeelshajahan212@gmail.com>
2026-08-09 15:33:10 +0530

    Customer

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -510,8 +510,8 @@ class OrderController extends Controller
                     'date'         => $order->created_at->format('d-m-Y'),
                     'time'         => $order->created_at->format('h:i A'),
                     'counter'      => $counter->name ?? '01',
-                    'customer_name' => $order->user?->customer?->name,
-                    'customer_contact' => $order->user?->customer?->contact_number ,
+                    'customer_name' => $order->user?->customer?->first_name . ' ' . $order->user?->customer?->last_name,
+                    'customer_contact' => $order->user?->customer?->phone_number ,
                     'customer_address' => $order->user?->customer?->address ,
                     'items'        => $order->items->map(fn ($orderItem) => [
                         'name'     => $orderItem->item_name ?? $orderItem->item?->name ?? 'Unknown Item',
